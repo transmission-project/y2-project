5c8448c007b5225a51958ed4733bc0da7f60e5e2
Paromita Banik <banikparomita111@gmail.com>
2019-04-24 16:43:51 +0200

    Added the group page

--- android/app/src/main/java/com/example/huntertalk/join_create.java
+++ android/app/src/main/java/com/example/huntertalk/join_create.java
@@ -1,17 +1,36 @@
 package com.example.huntertalk;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 
 public class join_create extends AppCompatActivity {
 
-
+    Button btjoin;
+    EditText etgroupid;
+    String st;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_join_create);
+        btjoin= findViewById(R.id.btjoin);
+        etgroupid = findViewById(R.id.etgroupid);
+
+        btjoin.setOnClickListener(new View.OnClickListener(){
+            public void onClick(View v){
+                Intent i=new Intent(join_create.this,group_page.class);
+                st = "Group id: " + etgroupid.getText().toString();
+                i.putExtra("Value",st);
+                startActivity(i);
+
+            }
+        });
+
 
     }
 }
