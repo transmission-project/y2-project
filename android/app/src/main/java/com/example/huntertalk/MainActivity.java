4fc49af3d28737faf43be8a768c7d1f5e1ffa7dd
Mikhail <mishav147@gmail.com>
2019-04-24 15:09:31 +0200

    Registration Activity connected with Login Activity w/o DB connections

--- android/app/src/main/java/com/example/huntertalk/MainActivity.java
+++ android/app/src/main/java/com/example/huntertalk/MainActivity.java
@@ -9,8 +9,6 @@ import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
 
-import com.google.firebase.auth.FirebaseAuth;
-
 public class MainActivity extends AppCompatActivity {
 
     @Override
