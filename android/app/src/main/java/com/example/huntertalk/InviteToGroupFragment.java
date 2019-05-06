5f2376974538ee5817f56bf291b7a012bf618272
Frida Mattsson <34581085+mattssonf@users.noreply.github.com>
2019-05-06 14:28:01 +0200

    add fragments to drawer menu and invite to group page

---
+++ android/app/src/main/java/com/example/huntertalk/InviteToGroupFragment.java
@@ -0,0 +1,21 @@
+package com.example.huntertalk;
+
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class InviteToGroupFragment extends Fragment {
+
+    View myView;
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        myView = inflater.inflate(R.layout.invite_to_group_layout, container, false);
+        return myView;
+    }
+}
