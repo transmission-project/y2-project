86a89f086db6ba6705489b2178ad206fe0dca6d4
hansikaat <44752144+hansikaat@users.noreply.github.com>
2019-05-07 10:52:27 +0200

    Invite a user and add the user to the invited table in the database

--- android/app/src/main/java/com/example/huntertalk/InviteToGroupFragment.java
+++ android/app/src/main/java/com/example/huntertalk/InviteToGroupFragment.java
@@ -5,6 +5,7 @@ import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.text.TextUtils;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -12,13 +13,17 @@ import android.widget.Button;
 import android.widget.EditText;
 
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 public class InviteToGroupFragment extends Fragment {
 
     View myView;
     private DatabaseReference userDb;
+    private DatabaseReference groupDb;
 
     @Nullable
     @Override
@@ -26,32 +31,70 @@ public class InviteToGroupFragment extends Fragment {
         myView = inflater.inflate(R.layout.invite_to_group_layout, container, false);
 
         super.onCreate(savedInstanceState);
-        getActivity().setContentView(R.layout.invite_to_group_layout);
+        //getActivity().setContentView(R.layout.invite_to_group_layout);
 
-        final EditText etSearch = getActivity().findViewById(R.id.etsearchmember);
-        Button searchButton = getActivity().findViewById(R.id.btnsearch);
+        final EditText etSearch = myView.findViewById(R.id.etsearchmember);
+        Button searchButton = myView.findViewById(R.id.btnsearch);
 
         userDb = FirebaseDatabase.getInstance().getReference("users");
+        groupDb = FirebaseDatabase.getInstance().getReference("groups");
 
         final FirebaseAuth auth = FirebaseAuth.getInstance();
         final String uid = auth.getCurrentUser().getUid();
 
+
+
         searchButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                String email = etSearch.getText().toString().trim();
+                final String enteredemail = etSearch.getText().toString().trim();
 
-                if (!email.contains("@") || !email.contains(".")) {
+                if (!enteredemail.contains("@") || !enteredemail.contains(".")) {
                     etSearch.setError("Invalid email addresss");
                     return;
                 }
-                if (TextUtils.isEmpty(email)) {
+                else if (TextUtils.isEmpty(enteredemail)) {
                     etSearch.setError("Enter Email");
                     return;
                 }
-                System.out.println("  Email checks passed");
+                else {
+
+                    //usersRef.child(uid).addValueEventListener(new ValueEventListener() {
+
+                    userDb.addValueEventListener(new ValueEventListener() {
+                        @Override
+                        public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                            for (DataSnapshot user: dataSnapshot.getChildren()){
+                                String dbemail=user.child("email").getValue().toString();
+
+                                String groupID;
+                                try {
+                                    groupID = getActivity().getIntent().getExtras().getString("groupID");
+                                }
+                                catch (NullPointerException e) {
+                                    groupID = "ERROR";
+                                }
+
+                                if(dbemail.equals(enteredemail)){
+                                    String inviteduser=user.getKey();
+                                    groupDb.child(groupID).child("invited").child(inviteduser).setValue(inviteduser);
+                                    //Log.d("grouptag", "groupID "+groupID);
+                                }
+
+                            }
+                        }
+
+                        @Override
+                        public void onCancelled(@NonNull DatabaseError databaseError) {
+
+                        }
+                    });
+
+                }
+
             }
             });
+
 
 
         return myView;
