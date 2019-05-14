75ab05a4658d4c6b369d131ae663eacddbe5b5f2
kolonia <alex.kolonia@hotmail.com>
2019-05-14 14:19:10 +0200

    Show other people in the map

--- android/app/src/main/java/com/example/huntertalk/everythingWithGroups/MapFragment.java
+++ android/app/src/main/java/com/example/huntertalk/everythingWithGroups/MapFragment.java
@@ -2,12 +2,10 @@ package com.example.huntertalk.everythingWithGroups;
 
 
 import android.annotation.SuppressLint;
-import android.content.Context;
 import android.location.Location;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.v4.app.Fragment;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -21,8 +19,6 @@ import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.CameraPosition;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
-import com.google.android.gms.tasks.OnCompleteListener;
-import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
@@ -36,11 +32,12 @@ public class MapFragment extends Fragment {
     private Location mLastKnownLocation;
     private Location mCurrentLocation;
     private CameraPosition mCameraPosition;
-    private float DEFAULT_ZOOM= 19;
+    private float DEFAULT_ZOOM= 3;
     private static final String KEY_CAMERA_POSITION = "camera_position";
     private static final String KEY_LOCATION = "location";
     private FusedLocationProviderClient mFusedLocationProviderClient;
     private  LatLng currentLocation;
+    private String nickname;
     private double latitude, longitude;
     private DatabaseReference groupRef;
     private String uid;
@@ -80,8 +77,7 @@ public class MapFragment extends Fragment {
             public void onMapReady(GoogleMap mMap1) {
                 mMap=mMap1;
                 mMap.setMapType(GoogleMap.MAP_TYPE_HYBRID);
-                mMap.setMyLocationEnabled(true);
-                groupRef.child(groupID).child("locations").child(uid).addListenerForSingleValueEvent(new ValueEventListener() {
+                groupRef.child(groupID).child("locations").child(uid).addValueEventListener(new ValueEventListener() {
                     @Override
                     public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
                         for(DataSnapshot parameters: dataSnapshot.getChildren()){
@@ -105,6 +101,50 @@ public class MapFragment extends Fragment {
 
                     }
                 });
+
+                //Show rest of the people on the map
+                groupRef.child(groupID).child("locations").addValueEventListener(new ValueEventListener() {
+                    @Override
+                    public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                        for(DataSnapshot people: dataSnapshot.getChildren()){
+                            if (!people.getKey().equals(uid)){
+
+                                for(DataSnapshot parameters: people.getChildren()){
+                                    System.out.println("The id is "+ people.getKey());
+
+                                    if(parameters.getKey().equals("latitude")){
+                                        latitude= Double.parseDouble(parameters.getValue().toString());
+                                    }
+                                    if(parameters.getKey().equals("longitude")){
+                                        longitude= Double.parseDouble(parameters.getValue().toString());
+                                    }
+                                }
+                                groupRef.child(groupID).child("joined").child(people.getKey().toString()).addListenerForSingleValueEvent(new ValueEventListener() {
+                                    @Override
+                                    public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                                        nickname = dataSnapshot.getValue().toString();
+                                        System.out.println("The nickname is "+ nickname);
+                                        mMap.addMarker(new MarkerOptions().position(new LatLng(latitude, longitude)).title(nickname.toUpperCase()));
+
+
+                                    }
+
+                                    @Override
+                                    public void onCancelled(@NonNull DatabaseError databaseError) {
+
+                                    }
+                                });
+                            }
+                        }
+                    }
+
+                    @Override
+                    public void onCancelled(@NonNull DatabaseError databaseError) {
+
+                    }
+                });
+
+
 
             }
         });
